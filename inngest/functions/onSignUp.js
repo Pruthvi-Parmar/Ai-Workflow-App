8b42183e6d6e084b18dbdf23afafee09a831aef5
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-06-01 04:20:18 +0530

    inngest worker added

---
+++ inngest/functions/onSignUp.js
@@ -0,0 +1,39 @@
+import { inngest } from "../client"
+import User from "../../models/user.model.js"
+import { NonRetriableError } from "inngest"
+import sendMail from "../../utils/mailer.js"
+
+const onUsersignup = inngest.createFunction(
+    {id: "on-user-signup", retries: 2},
+    {event: "user/signup"},
+
+    async ({event, step}) => {
+        try {
+            const { email } = event.data
+            const user = await step.run("get-user-email",async () => {
+                const userObject = await User.finfOne({email})
+                if(!userObject){
+                    throw new NonRetriableError("User no longer exist in database")
+                }
+                return userObject
+            })
+
+            await step.run("send-welcome-email",async () => {
+                const subject = `Welcome to the app`
+                const message = `Hey,
+                \n\n
+                Thanks for signup. We're glad to have you onboard!
+                `
+
+                await sendMail(user.email, subject, message)
+            })
+
+            return {
+                success: true
+            }
+        } catch (error) {
+            console.log("Error running steps",error.message);
+            
+        }
+    }
+)
